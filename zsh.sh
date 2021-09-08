8f1ffbf7828afdddae5d0a995cea7b7b37781c70
HXLLL <1150189655@qq.com>
2021-09-08 08:15:46 +0800

    zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -3,7 +3,9 @@
 apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-sed -i .zshrc -e "s/^ZSH_THEME.*$/ZSH_THEME=powerlevel10k/powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+sed -i .zshrc -e "s/^ZSH_THEME.*$/ZSH_THEME=powerlevel10k\/powerlevel10k/"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
